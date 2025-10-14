6146e082a8ca2f8d1fe761760b3241a04794ac36
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-10-14 16:54:32 +0600

    discount none

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -6,34 +6,38 @@ import Menu from "../navbar/menu/Menu";
 import Discount from "./discount/Discount";
 import Header from "../navbar/header/Header";
 import { usePathname } from "next/navigation";
+import { useGetLaptopQuery } from "@/redux/api/laptop";
 
 export interface FilterState {
-	[key: string]: (string | number)[];
+  [key: string]: (string | number)[];
 }
 
 const HomePage = () => {
-	const [selectedFilters, setSelectedFilters] = useState<FilterState>({});
-	const pathname = usePathname();
-	return (
-		<div className="w-full">
-			{pathname === "/" && (
-				<Header
-					selectedFilters={selectedFilters}
-					setSelectedFilters={setSelectedFilters}
-				/>
-			)}
-			<div className="flex w-full">
-				<Menu
-					selectedFilters={selectedFilters}
-					setSelectedFilters={setSelectedFilters}
-				/>
-				<div className="w-full overflow-hidden">
-					<Discount />
-					<Hero selectedFilters={selectedFilters} />
-				</div>
-			</div>
-		</div>
-	);
+  const [selectedFilters, setSelectedFilters] = useState<FilterState>({});
+  const pathname = usePathname();
+  const { data } = useGetLaptopQuery();
+  const hasDiscount = data?.some((laptop) => laptop.discount > 0);
+
+  return (
+    <div className="w-full">
+      {pathname === "/" && (
+        <Header
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+        />
+      )}
+      <div className="flex w-full">
+        <Menu
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+        />
+        <div className="w-full overflow-hidden">
+          {hasDiscount && <Discount />}
+          <Hero selectedFilters={selectedFilters} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
